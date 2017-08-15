402b19cadc2127534ad54b8869984be0b8fe735b
rkravikant <30904146+rkravikant@users.noreply.github.com>
2017-08-15 13:15:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/check.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'check.html'));
+});
+
+app.get('/ui/ab.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ab.jpg'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
