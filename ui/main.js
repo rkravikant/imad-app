3163252550e1a6d4191e2ba73176cad054965290
rkravikant <30904146+rkravikant@users.noreply.github.com>
2017-08-20 16:27:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,6 @@ button.onclick = function() {
                                             }
                                         }
                                     };
-                        request.open('GET','http://ravikantvermahbti.imad.hasura-app.io/counter',true);
+                        request.open('GET', 'http://ravikantvermahbti.imad.hasura-app.io/counter', true);
                         request.send(null);
 };
