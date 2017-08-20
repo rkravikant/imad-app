0b12b66338b68135a72d32865de9cbf1d159db29
rkravikant <30904146+rkravikant@users.noreply.github.com>
2017-08-20 15:45:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,26 @@
 var button=document.getElementById('counter');
 
 button.onclick = function()
-{
+{       var counter=0;
+/*
     var request = new XMLHttpRequest();
+    
     
     request.onreadystatechange = function()
                                     {
-                                        if(request.readystate===XMLHttpRequest.DONE)
+                                       if(request.readystate===XMLHttpRequest.DONE)
                                         {
                                             if(request.status===200)
-                                            {
-                                             counter = request.responseText;
+                                            { 
+                                             counter = request.responseText;  */
+                                              counter=counter+1;
                                              var span = document.getElementById('span');
                                               span.innerHTML = counter.toString();
-                                            }
-                                        }
+                                            
+                                    
                                     };
 
-request.open('GET','http://ravikantvermahbti.imad.hasura-app.io/counter',true);
-request.send(null);
+//request.open('GET','http://ravikantvermahbti.imad.hasura-app.io/counter',true);
+//request.send(null);
 
 };
