4cb6f2a3136c501f99bc912a9583d0bbf10505bc
rkravikant <30904146+rkravikant@users.noreply.github.com>
2017-08-20 16:08:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:articlename', function (req, res) {
     var articlename=req.params.articlename; // there is one more method for taking params by 
     
