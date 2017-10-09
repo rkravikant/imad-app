415af397af299b4eceb5f9910de5cb6ac1b7d3e3
rkravikant <30904146+rkravikant@users.noreply.github.com>
2017-10-09 22:36:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyparser = require('body-parser');
 
 var config={
     user:'ravikantvermahbti',
@@ -14,6 +15,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyparser.json()); // loading json content to req body
 
 function createtemplate(object)
 {
@@ -134,9 +136,61 @@ app.get('/articles/:articlename', function (req, res) {
   
 });
 
+app.post('/create-user', function(req, res){ // we dont send password by get because it will be printed in logs, if one has acces to logs can see password
+// extracting username and password from request body
+    var username = req.body.username;
+    var password = req.body.password;
+// now where is this data comming from in req body and what is format of data, lets take json type, so we have to tell express app to when it see the json content in request it will put it into req body that is done with the help of body parser.
+   
+//now for making post request we have to do it from main.js but for practice we can do it from curl tool, curl will print the whole html in cmd(if we use from ssh) like browser rather than rendering.we can see whole detail like which page is being requested and what protocols, req method being used. so by curl we can send post request(with data) without any page(web-html..)
+
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbstring = hash(password, salt);
+    
+    pool.query("insert into 'user' (password, username) values ($1,$2)", [username, dbstring], function (err, result){ // "user" because user is also a keyword in postgress
+    if(err){                           // this is secure way, not allows sql injection, which was in upper case // and there values in array []
+    res.status(500).send(err.toString());
+    }
+    else{
+            res.send("user succesfully created"+username);
+        }
+    
+});
+});
+
+app.post('/login', function(req, res){
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var dbstring = hash(password, salt);
+    
+    pool.query("select * from 'user' where username =$1)", [username], function (err, result){
+    if(err){                         
+    res.status(500).send(err.toString());
+    }
+    else{
+          if(result.rows[0].length() === 0){
+            res.status(404).send("Requested user name is not registered");
+            }
+            else{
+            var dbstring = result.rows[0].password;
+              var salt = dbstring.split("$")[2]; // salt is 3rd element in array
+              var hashedpassword = hash(password,salt);
+            if(dbstring === hashedpassword)  
+            res.send("credential matched   Welcome");
+            else{
+                res.status(404).send("Entered password is incorrect");
+            }
+        }
+    }
+    
+});
+});
+
 function hash(input, salt){
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex'); // converting reurned bytes to readable string using hexadecimal encoding
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');// will apend input to salt value and convert it into 512byte string by applying 10000 times this hash funion.
+    return ["pbkdf2", "10000", "salt", "hashed.toString('hex')"].join($); // converting reurned bytes to readable string using hexadecimal encoding
     
 }
 
