86b244319eca50817fe39f035ba09834b1e6d3df
rkravikant <30904146+rkravikant@users.noreply.github.com>
2017-10-26 14:06:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ app.post('/login', function(req, res){
 
 function hash(input, salt){
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');// will apend input to salt value and convert it into 512byte string by applying 10000 times this hash funion.
-    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join($); // converting reurned bytes to readable string using hexadecimal encoding
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$'); // converting reurned bytes to readable string using hexadecimal encoding
     
 }
 
